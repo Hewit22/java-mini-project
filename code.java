747afbe8a5466b1b2d00dba75a91da7f65c15429
Hewit22 <hewitajith2021@gmail.com>
2026-04-13 23:49:52 +0530

    Update code.java

--- code.java
+++ code.java
@@ -23,30 +23,79 @@ class Question {
     }
 }
 
-public class Main {   // ✅ ONLY ONE public class
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int score = 0;
+
         Question q1 = new Question(
-                "What is Java?",
-                "Programming Language",
-                "Operating System",
-                "Browser",
-                "Hardware",
-                1
+                "What is the capital of India?",
+                "Mumbai",
+                "Delhi",
+                "Chennai",
+                "Kolkata",
+                2
         );
 
-        int score = 0;
+        Question q2 = new Question(
+                "Which planet is known as the Red Planet?",
+                "Earth",
+                "Mars",
+                "Jupiter",
+                "Venus",
+                2
+        );
+
+        Question q3 = new Question(
+                "Who is known as the Father of Computers?",
+                "Albert Einstein",
+                "Isaac Newton",
+                "Charles Babbage",
+                "Nikola Tesla",
+                3
+        );
+
+        Question q4 = new Question(
+                "Which is the largest ocean in the world?",
+                "Atlantic Ocean",
+                "Indian Ocean",
+                "Arctic Ocean",
+                "Pacific Ocean",
+                4
+        );
+
+        Question q5 = new Question(
+                "Which country is famous for the Eiffel Tower?",
+                "Italy",
+                "Germany",
+                "France",
+                "Spain",
+                3
+        );
+
 
         q1.display();
         System.out.print("Enter answer: ");
-        int ans = sc.nextInt();
+        if (sc.nextInt() == q1.correctAnswer) score++;
 
-        if (ans == q1.correctAnswer) {
-            score++;
-        }
+        q2.display();
+        System.out.print("Enter answer: ");
+        if (sc.nextInt() == q2.correctAnswer) score++;
 
-        System.out.println("Score: " + score);
+        q3.display();
+        System.out.print("Enter answer: ");
+        if (sc.nextInt() == q3.correctAnswer) score++;
+
+        q4.display();
+        System.out.print("Enter answer: ");
+        if (sc.nextInt() == q4.correctAnswer) score++;
+
+        q5.display();
+        System.out.print("Enter answer: ");
+        if (sc.nextInt() == q5.correctAnswer) score++;
+
+        System.out.println("\nYour Final Score: " + score + "/5");
 
         sc.close();
     }
